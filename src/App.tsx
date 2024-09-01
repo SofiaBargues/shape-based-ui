005047292ad8534a286faa4275bf57dab8c5f5c3
Sofia Bargues <barguessofia@gmail.com>
2024-09-01 16:13:03 +0100

    is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -10,11 +10,24 @@ const BOX_DATA = [
 
 function App() {
   const [numsClicked, setNumsClicked] = useState<number[]>([]);
+  // necesito una variable booleana que sepa si ha sido completadp o no el patron
+  const [completed, isCompleted] = useState(false);
   const rowLength = BOX_DATA[0].length;
+
+  //doble forloop para saber el largo ( canftidad de cajaas con UNOS )
+  let boxNum = 7;
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      if (numsClicked.length > 0 && completed == true) {
+        setNumsClicked(numsClicked.slice(1));
+      }
+    }, 19000 / 100);
+    return () => clearTimeout(timeId);
+  }, [completed, numsClicked]);
 
   return (
     <>
-      <div>hello</div>
       {BOX_DATA.map((row, i) => (
         <div className="flex">
           {row.map((cell, j) => {
@@ -26,15 +39,16 @@ function App() {
                     const arrIsClicked = [...numsClicked];
                     arrIsClicked.push(mainIndex);
                     setNumsClicked(arrIsClicked);
+                    isCompleted(arrIsClicked.length === boxNum);
                     console.log(mainIndex);
                   }}
                   className={
-                    "border-2 border-black h-10 w-10 " +
+                    "border-2  border-black h-10 w-10 " +
                     (numsClicked.includes(mainIndex) ? "bg-lime-700" : "")
                   }
                   disabled={numsClicked.includes(mainIndex)}
                 >
-                  {mainIndex}
+                  {/* {mainIndex} */}
                 </button>
               );
             } else {
